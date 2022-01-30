a876afac71899b3ebcf85af82299e5f9b4472bb5
Sercan Özen <srcn.ozn5@gmail.com>
2022-01-31 00:44:29 +0300

    first upload

---
+++ projects/laravelProject/database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Faker\Factory;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $factory = Factory::create('en');
+        for ($i=0; $i<100; $i++)
+        {
+
+        }
+
+
+    }
+}
